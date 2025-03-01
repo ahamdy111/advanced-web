565dbd9c1f16b9997c305c67bfef513ba58049b8
ahamdy111 <164859823+ahamdy111@users.noreply.github.com>
2025-03-01 17:10:14 +0200

    Add files via upload
    Assignment #1

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+//exercise1
+$x = 10;
+$y = 7;
+echo "$x + $y = " . ($x + $y) . "<br>";
+echo "$x - $y = " . ($x - $y) . "<br>";
+echo "$x * $y = " . ($x * $y) . "<br>";
+echo "$x / $y = " . ($x / $y) . "<br>";
+echo "$x % $y = " . ($x % $y) . "<br>";
+
+//exercise2
+// $month = date('F', time());
+// if ($month == "August") {
+//     echo "It's August, so it's really hot.";
+// } else {
+//     echo "Not August, so at least not in the peak of the heat.";
+// }
+
+//exercise3
+// for ($i = 1; $i <= 12; $i++) {
+//     echo "$i * $i = " . ($i * $i) . "<br>";
+// }
+
+//exercise4
+// echo "<table border='1' cellspacing='0' cellpadding='10'>";
+
+// for ($i = 1; $i <= 7; $i++) {
+//     echo "<tr>";
+//     for ($j = 1; $j <= 7; $j++) {
+//         echo "<td>" . ($i * $j) . "</td>";
+//     }
+//     echo "</tr>";
+// }
+
+// echo "</table>";
+
+
+
+
+
+?>
